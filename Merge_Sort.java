3d18b0a0478d9a6add5c2a8343a727915a8d77a9
superhotdogzz <superhotdogzz@gmail.com>
2017-01-28 14:38:36 -0800

    The set up, putting a java file for fun

---
+++ Merge_Sort.java
@@ -0,0 +1,23 @@
+package Lab02;
+
+public class Merge_Sort {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static <E extends Comparable<E>> void mergesort(E[] list)
+	{
+		
+	}
+	
+	public static <E extends Comparable<E>> void mergesort(E[]list, int left,int right)
+	{
+		if(left<right)
+		{
+			int center = (left+right)/2;
+			mergesort();
+		}
+	}
+}
